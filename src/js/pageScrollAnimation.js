0f6095ffdacab63c8334f2a470ea08c5f94f7d08
Jul Bar <barankovauv@gmail.com>
2023-01-26 23:12:05 +0300

    footer lines animation

--- src/js/pageScrollAnimation.js
+++ src/js/pageScrollAnimation.js
@@ -1,8 +1,8 @@
 function pageScrollAnimation() {
   const sections = document.querySelectorAll('section');
 
-
   // Prices section lines
+  // после CMS неизвестно сколько будет цен, поэтому полоски динамечески добавим на все
   const priceItems = document.querySelectorAll('.pricelist__item');
   if (priceItems && priceItems.length > 0) {
     priceItems.forEach(item => {
@@ -26,16 +26,31 @@ function pageScrollAnimation() {
         borderContainer.append(borderLine);
       }
     })
-   }
-  
+  }
+
+  // Footer lines
+  const footer = document.querySelector('.footer');
+  const footerBlocks = document.querySelectorAll('.footer__block');
+  if (footer) {
+      const topLine = document.createElement('div');
+      topLine.classList.add('footer__top-border');
+      footer.append(topLine);
+  }
+  if (footerBlocks && footerBlocks.length > 0) {
+    footerBlocks.forEach(item => {
+      const rihghtBorder = document.createElement('div');
+      rihghtBorder.classList.add('footer__block-border');
+      item.append(rihghtBorder);
+    })
+  }
 
   function scrollAnimation() {
     sections.forEach(el => {
       let scroll = window.pageYOffset;
-      let top = el.offsetTop - (0.5 * el.clientHeight);
+      let top = el.offsetTop - (0.5 * el.offsetHeight);
       let bottom = el.offsetHeight + top;
       let id = el.getAttribute('id');
-
+      
       if (scroll > top && scroll < bottom)  {
         // анимации линий каждого прайс элемента при скролле
         if (id === 'prices' && priceItems?.length > 0) {
@@ -51,7 +66,14 @@ function pageScrollAnimation() {
           }
         }
       }
+      
     });
+
+    // анимации бордера в footer
+    if (footer) {
+      if (window.pageYOffset > footer.offsetTop - 2 * footer.offsetHeight)
+      footer.classList.add('scroll-animation');
+    }
   }
 
   if (sections?.length > 0) {
